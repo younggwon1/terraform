c49882de70024b3fe0625a6ba1ad6df428f217c7
younggwon1 <dlrjsapdlf622@naver.com>
2025-01-19 14:35:39 +0900

    modify terraform version range & aws version

--- network/variables.tf
+++ network/variables.tf
@@ -39,10 +39,12 @@ variable "private-subnets-cidr" {
 }
 
 terraform {
+  required_version = ">= 1.3.2"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 5.83.0"
     }
   }
 
