cd8efe724a7bb65f5328c23eac299b1babc1f358
younggwon1 <dlrjsapdlf622@naver.com>
2025-11-16 20:42:42 +0900

    feat(terraform): Create ACM

---
+++ acm/variables.tf
@@ -0,0 +1,34 @@
+variable "domain_name" {
+  description = "Domain Name"
+  type        = string
+  default     = "test.com"
+}
+
+terraform {
+  required_version = ">= 1.10.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.20.0"
+    }
+  }
+
+  backend "s3" {
+    bucket       = "tf-remote-state"
+    key          = "ap-northeast-2.acm.tfstate"
+    region       = "ap-northeast-2"
+    use_lockfile = true
+    encrypt      = true
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-2"
+
+  default_tags {
+    tags = {
+      ManagedBy = "Terraform"
+    }
+  }
+}
